9df1e94c3d2a590c547d353f455b019fca9c117e
Mitch Chapman <mchapman87501@gmail.com>
2019-10-21 07:37:38 -0600

    Clean up.

--- app/model/earth_geom.py
+++ app/model/earth_geom.py
@@ -15,24 +15,24 @@ class EarthGeom:
         zone_width_deg = 90.0 / num_zones
         # Midpoint latitudes in degrees:
         zw2 = zone_width_deg / 2.0
+
+        # Central angle of each latitude band, degrees, for
+        # one hemisphere:
         lat_degs = np.arange(zw2, 90.0, zone_width_deg)
+
+        # Ditto, radians:
         self.lats_rad = lat_degs * np.pi / 180.0
         # One half of the width of each zone, in rads:
         delta_rad = zw2 * np.pi / 180.0
-        # Assume earth is a ... cylinder?!  Get the fraction of
-        # the surface covered by each latitude zone.
-        self.lats_frac = np.sin(self.lats_rad + delta_rad) - np.sin(
-            self.lats_rad - delta_rad
-        )
 
         # Treat earth as a stack of cylinders.  The radius of each
         # cylinder corresponds to the (unit) radius of the earth
         # sphere at the central latitude of each band.
-        self.lats_radius = np.cos(self.lats_rad)
+        lats_radius = np.cos(self.lats_rad)
         self.lats_height = np.sin(self.lats_rad + delta_rad) - np.sin(
             self.lats_rad - delta_rad
         )
-        lats_area = self.lats_radius * self.lats_height  # * 2.0 * np.pi
+        lats_area = lats_radius * self.lats_height  # * 2.0 * np.pi
 
         total_area = sum(lats_area)
         self.lats_frac = lats_area / total_area
